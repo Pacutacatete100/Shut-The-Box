fd3ab42b2d33e5bfc332ae4ca034365361d281fd
Pacutacatete100 <pacutacatete100@gmail.com>
2020-06-05 01:25:25 -0500

    error handling

--- GameVariants.py
+++ GameVariants.py
@@ -12,7 +12,13 @@ def play_base_game():
         print('\nYou rolled: ' + str(roll))
         if not box.combination_not_possible(roll):
             choices = ask_numbers_to_remove()
-            box.remove_pieces(choices)
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
         elif box.combination_not_possible(roll):
             print('Sorry! you\'re all out of luck! Final score is: ' +
                   str(sum(box.pieces)))
@@ -37,7 +43,13 @@ def two_to_go():
         print('\nYou rolled: ' + str(roll))
         if not box.combination_not_possible(roll):
             choices = ask_numbers_to_remove()
-            box.remove_pieces(choices)
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
         elif box.combination_not_possible(roll):
             print('Sorry! you\'re all out of luck! Final score is: ' +
                   str(sum(box.pieces)))
@@ -48,7 +60,7 @@ def three_to_go():
     box = Box([1, 2, 4, 5, 6, 7, 8, 9])
     playing = True
 
-    print('2 to go: the 2 is automatically removed, if you roll 4 on your first roll, you lose the game')
+    print('3 to go: the 3 is automatically removed, if you roll 4 on your first roll, you lose the game')
 
     roll = roll_die(box.find_max_piece())
     if roll == 4:
@@ -62,7 +74,13 @@ def three_to_go():
         print('\nYou rolled: ' + str(roll))
         if not box.combination_not_possible(roll):
             choices = ask_numbers_to_remove()
-            box.remove_pieces(choices)
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
         elif box.combination_not_possible(roll):
             print('Sorry! you\'re all out of luck! Final score is: ' +
                   str(sum(box.pieces)))
@@ -81,7 +99,88 @@ def three_down_extreme():
         print('\nYou rolled: ' + str(roll))
         if not box.combination_not_possible(roll):
             choices = ask_numbers_to_remove()
-            box.remove_pieces(choices)
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
+        elif box.combination_not_possible(roll):
+            print('Sorry! you\'re all out of luck! Final score is: ' +
+                  str(sum(box.pieces)))
+            playing = False
+
+
+def against_all_odds():
+    box = Box([1, 3, 5, 7, 9])
+    playing = True
+
+    print('only odd numbers are up')
+
+    while playing:
+        box.print_remaining_pieces()
+        roll = roll_die(box.find_max_piece())
+        print('\nYou rolled: ' + str(roll))
+        if not box.combination_not_possible(roll):
+            choices = ask_numbers_to_remove()
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
+        elif box.combination_not_possible(roll):
+            print('Sorry! you\'re all out of luck! Final score is: ' +
+                  str(sum(box.pieces)))
+            playing = False
+
+
+def even_stevens():
+    box = Box([2, 4, 6, 8])
+    playing = True
+
+    print('only even numbers are up')
+
+    while playing:
+        box.print_remaining_pieces()
+        roll = roll_die(box.find_max_piece())
+        print('\nYou rolled: ' + str(roll))
+        if not box.combination_not_possible(roll):
+            choices = ask_numbers_to_remove()
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
+        elif box.combination_not_possible(roll):
+            print('Sorry! you\'re all out of luck! Final score is: ' +
+                  str(sum(box.pieces)))
+            playing = False
+
+
+def full_house():
+    box = Box([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    playing = True
+
+    print('12 numbers are up')
+
+    while playing:
+        box.print_remaining_pieces()
+        roll = roll_die(box.find_max_piece())
+        print('\nYou rolled: ' + str(roll))
+        if not box.combination_not_possible(roll):
+            choices = ask_numbers_to_remove()
+            if sum(choices) == roll:
+                box.remove_pieces(choices)
+                if len(choices) == 0:
+                    print('YOU WIN')
+                    playing = False
+            elif sum(choices) != roll:
+                print('doesnt add up')
         elif box.combination_not_possible(roll):
             print('Sorry! you\'re all out of luck! Final score is: ' +
                   str(sum(box.pieces)))
--- Main.py
+++ Main.py
@@ -5,4 +5,27 @@ from GameUtils import *
 from GameVariants import *
 
 if __name__ == "__main__":
-    two_to_go()
+    print('1. Normal Game')
+    print('2. 2 To Go: the 2 is automatically removed, if you roll 4 on your first roll, you lose the game')
+    print('3. 3 To To: the 3 is automatically removed, if you roll 4 on your first roll, you lose the game')
+    print('4. 3 Down Extreme: numbers 1, 2 and 3 are pre-dropped, leaving numbers 4 to 9 up')
+    print('5. Against All Odds: only the odd numbers are up')
+    print('6. Even-Stevens: only the even numbers are up')
+    print('7. Full House: numbers 1 - 12 are up')
+
+    variant = input('chose what version of the game you want to play: ')
+
+    if int(variant) == 1:
+        play_base_game()
+    elif int(variant) == 2:
+        two_to_go()
+    elif int(variant) == 3:
+        three_to_go()
+    elif int(variant) == 4:
+        three_down_extreme()
+    elif int(variant) == 5:
+        against_all_odds()
+    elif int(variant) == 6:
+        even_stevens()
+    elif int(variant) == 7:
+        full_house()
