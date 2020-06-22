3a9120be31a2227b41640688a9c8419c6259795d
Pacutacatete100 <pacutacatete100@gmail.com>
2020-06-22 01:15:08 -0500

    update UI

--- View/GameGUI.py
+++ View/GameGUI.py
@@ -3,28 +3,34 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
-def clicked():
-    print('clicked')
+class StartUI(QMainWindow):
+    def __init__(self):
+        super(StartUI, self).__init__()
+        self.setGeometry(300, 300, 500, 400)
+        self.setWindowTitle("Shut The Box")
+
+        self.initUI()
+
+    def initUI(self):
+        self.single = QtWidgets.QPushButton(self)
+        self.single.setText("Single Player")
+        self.single.move(125, 100)
+        self.single.resize(250, 50)
+        self.single.clicked.connect(self.clicked)
+
+        self.multi = QtWidgets.QPushButton(self)
+        self.multi.setText("Multiplayer")
+        self.multi.move(125, 250)
+        self.multi.resize(250, 50)
+        self.multi.clicked.connect(self.clicked)
+
+    def clicked(self):
+        print('clicked')
 
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(300, 300, 500, 400)
-    win.setWindowTitle("Shut The Box")
-
-    single = QtWidgets.QPushButton(win)
-    single.setText("Single Player")
-    single.move(125, 100)
-    single.resize(250, 50)
-    single.clicked.connect(clicked)
-
-    multi = QtWidgets.QPushButton(win)
-    multi.setText("Multiplayer")
-    multi.move(125, 250)
-    multi.resize(250, 50)
-    multi.clicked.connect(clicked)
-
+    win = StartUI()
     win.show()
     sys.exit(app.exec_())
 
